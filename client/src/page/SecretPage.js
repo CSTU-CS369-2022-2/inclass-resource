afb29567502161521f0bbc729f88c504ca118d4c
poompichayout <poompichayout@gmail.com>
2023-04-24 23:42:53 +0700

    week11: client side (step 7)

--- client/src/page/SecretPage.js
+++ client/src/page/SecretPage.js
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react'
+
+//import { useAuth } from "../utils/AuthProvider";
+import useFetchPrivate from '../utils/useFetchPrivate'
+
+import Spinner from '../component/Spinner'
+
 export default function SecretPage() {
-	return <h1>Very Secret Page!</h1>
+	// const auth = useAuth()
+	const [secretData, setData] = useState()
+
+	const { loading, callFetch } = useFetchPrivate()
+
+	useEffect(() => {
+		const getSecretData = async () => {
+			const { response, data } = await callFetch('/secret')
+			if (response.ok) {
+				setData(JSON.stringify(data))
+			}
+		}
+		try {
+			getSecretData()
+		} catch (error) {
+			console.error(error)
+			throw error
+		}
+	}, [callFetch])
+
+	return (
+		<>
+			<h1>Very Secret Page!</h1>
+			{loading ? <Spinner /> : <div>Content from the server: {secretData}</div>}
+		</>
+	)
 }
--- client/src/utils/AuthProvider.js
+++ client/src/utils/AuthProvider.js
@@ -3,20 +3,27 @@ import { createContext, useState, useContext } from 'react'
 const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
-	const [user, setUser] = useState({})
+	// const [user, setUser] = useState({})
+	let [user, setUser] = useState(() =>
+		sessionStorage.getItem('aToken')
+			? JSON.parse(sessionStorage.getItem('aToken'))
+			: {}
+	)
+
 	let signin = (newUser, callback) => {
 		setUser(newUser)
-		console.log('sign in', newUser)
+		sessionStorage.getItem('aToken', newUser)
 		callback()
 	}
 
 	let signout = (callback) => {
+		if (sessionStorage.getItem('aToken')) sessionStorage.clear()
 		setUser({})
 		callback()
 	}
 
 	return (
-		<AuthContext.Provider value={{ user, signin, signout }}>
+		<AuthContext.Provider value={{ user, setUser, signin, signout }}>
 			{children}
 		</AuthContext.Provider>
 	)
